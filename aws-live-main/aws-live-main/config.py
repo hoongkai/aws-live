e6f7a669cb21ad1482f299aa5530929284fc1d1f
Hoong Kai Chan <hkchan.fl@gmail.com>
2022-03-31 23:06:28 +0800

    Delete aws-live-main/aws-live-main directory

--- aws-live-main/aws-live-main/config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.cxdymafgd8jd.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee"
-customregion = "us-east-1"
